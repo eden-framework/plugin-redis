7bab2ff1158e1d04c478c36e1bc298dbfdc3ae0c
liyiwen <johnnyeven@gmail.com>
2020-09-24 15:36:03 +0800

    init

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ replace k8s.io/client-go => k8s.io/client-go v0.18.8
 
 require (
 	github.com/eden-framework/eden-framework v1.0.12 // indirect
+	github.com/go-redis/redis/v8 v8.2.1 // indirect
 	github.com/gomodule/redigo v1.8.2 // indirect
 )
--- redis.go
+++ redis.go
@@ -1,8 +1,9 @@
 package redis
 
 import (
+	"context"
 	"fmt"
-	"github.com/gomodule/redigo/redis"
+	"github.com/go-redis/redis/v8"
 	"github.com/profzone/envconfig"
 	"time"
 )
@@ -13,16 +14,15 @@ type Redis struct {
 	Port           int
 	User           string
 	Password       envconfig.Password
+	MaxRetries     int
 	ConnectTimeout envconfig.Duration
 	ReadTimeout    envconfig.Duration
 	WriteTimeout   envconfig.Duration
 	IdleTimeout    envconfig.Duration
-	MaxActive      int
-	MaxIdle        int
-	Wait           bool
+	MinIdle        int
 	DB             int
 
-	client *redis.Pool
+	*redis.Client `ignored:"true"`
 }
 
 func (r *Redis) SetDefaults() {
@@ -31,6 +31,9 @@ func (r *Redis) SetDefaults() {
 	}
 	if r.Port == 0 {
 		r.Port = 6379
+	}
+	if r.MaxRetries == 0 {
+		r.MaxRetries = 3
 	}
 	if r.ConnectTimeout == 0 {
 		r.ConnectTimeout = envconfig.Duration(10 * time.Second)
@@ -42,16 +45,7 @@ func (r *Redis) SetDefaults() {
 		r.WriteTimeout = envconfig.Duration(10 * time.Second)
 	}
 	if r.IdleTimeout == 0 {
-		r.IdleTimeout = envconfig.Duration(240 * time.Second)
-	}
-	if r.MaxActive == 0 {
-		r.MaxActive = 5
-	}
-	if r.MaxIdle == 0 {
-		r.MaxIdle = 3
-	}
-	if !r.Wait {
-		r.Wait = true
+		r.IdleTimeout = envconfig.Duration(5 * time.Minute)
 	}
 	if r.DB == 0 {
 		r.DB = 10
@@ -59,91 +53,35 @@ func (r *Redis) SetDefaults() {
 }
 
 func (r *Redis) Init() {
-	if r.client != nil {
+	if r.Client != nil {
 		return
 	}
 
-	dialFunc := func() (c redis.Conn, err error) {
-		c, err = redis.Dial(
-			r.Protocol,
-			fmt.Sprintf("%s:%d", r.Host, r.Port),
+	r.SetDefaults()
+	r.Client = redis.NewClient(&redis.Options{
+		Network:      r.Protocol,
+		Addr:         fmt.Sprintf("%s:%d", r.Host, r.Port),
+		Username:     r.User,
+		Password:     r.Password.String(),
+		DB:           r.DB,
+		MaxRetries:   r.MaxRetries,
+		DialTimeout:  time.Duration(r.ConnectTimeout),
+		ReadTimeout:  time.Duration(r.ReadTimeout),
+		WriteTimeout: time.Duration(r.WriteTimeout),
+		MinIdleConns: r.MinIdle,
+		IdleTimeout:  time.Duration(r.IdleTimeout),
+		TLSConfig:    nil,
+		Limiter:      nil,
+	})
 
-			redis.DialWriteTimeout(time.Duration(r.WriteTimeout)),
-			redis.DialConnectTimeout(time.Duration(r.ConnectTimeout)),
-			redis.DialReadTimeout(time.Duration(r.ReadTimeout)),
-			redis.DialUsername(r.User),
-			redis.DialPassword(r.Password.String()),
-			redis.DialDatabase(r.DB),
-		)
-		return c, err
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	_, err := r.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("redis Ping err:%v", err))
 	}
-
-	r.client = &redis.Pool{
-		Dial:        dialFunc,
-		MaxIdle:     r.MaxIdle,
-		MaxActive:   r.MaxActive,
-		IdleTimeout: time.Duration(r.IdleTimeout),
-		Wait:        true,
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			if time.Since(t) < 5*time.Minute {
-				return nil
-			}
-			_, err := c.Do("PING")
-			return err
-		},
-	}
-}
-
-func (r *Redis) Close() error {
-	return r.client.Close()
 }
 
 func (r *Redis) Prefix(key string) string {
 	return fmt.Sprintf("%s:%s", "prefix", key)
-}
-
-func (r *Redis) Get() redis.Conn {
-	if r.client != nil {
-		return r.client.Get()
-	}
-	return nil
-}
-
-func (r *Redis) Query(cmd *CMD, others ...*CMD) (interface{}, error) {
-	c := r.Get()
-	defer c.Close()
-
-	if (len(others)) == 0 {
-		return c.Do(cmd.name, cmd.args...)
-	}
-
-	err := c.Send("MULTI")
-	if err != nil {
-		return nil, err
-	}
-
-	err = c.Send(cmd.name, cmd.args...)
-	if err != nil {
-		return nil, err
-	}
-
-	for i := range others {
-		o := others[i]
-		if o == nil {
-			continue
-		}
-		err := c.Send(o.name, o.args...)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return c.Do("EXEC")
-}
-
-func (r *Redis) Execute(cmd *CMD) error {
-	c := r.Get()
-	defer c.Close()
-
-	return c.Send(cmd.name, cmd.args...)
 }
