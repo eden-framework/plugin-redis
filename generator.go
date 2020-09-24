dbd1fa2ee2b710010c2cf9ef50293e64d7d37995
liyiwen <johnnyeven@gmail.com>
2020-09-24 17:00:17 +0800

    init

--- generator.go
+++ generator.go
@@ -1,20 +1,35 @@
 package redis
 
 import (
+	"fmt"
 	"github.com/eden-framework/eden-framework/pkg/generator"
 	"github.com/eden-framework/eden-framework/pkg/generator/files"
+	"path"
 )
 
 type GenerationPlugin struct {
 }
 
-func (g *GenerationPlugin) NewApplicationGenerationPoint(opt generator.ServiceOption) string {
-	panic("implement me")
+func (g *GenerationPlugin) NewApplicationGenerationPoint(opt generator.ServiceOption, cwd string) string {
+	globalPkgPath := path.Join(opt.PackageName, "internal/global")
+	globalFilePath := path.Join(cwd, "internal/global")
+	tpl := fmt.Sprintf(`,
+		{{ .UseWithoutAlias "github.com/eden-framework/eden-framework/pkg/application" "" }}.WithConfig(&{{ .UseWithoutAlias "%s" "%s" }}.RedisConfig)`, globalPkgPath, globalFilePath)
+	return tpl
 }
 
-func (g *GenerationPlugin) FileGenerationPoint(opt generator.ServiceOption) *files.GoFile {
+func (g *GenerationPlugin) FileGenerationPoint(opt generator.ServiceOption, cwd string) *files.GoFile {
 	file := files.NewGoFile("global")
-	file.WithBlock(``)
+	file.WithBlock(`
+var RedisConfig = struct {
+	Redis *{{ .UseWithoutAlias "github.com/eden-framework/redis" "" }}.Redis
+}{
+	Redis: &{{ .UseWithoutAlias "github.com/eden-framework/redis" "" }}.Redis{
+		Host: "localhost",
+		Port: 6379,
+	},
+}
+`)
 
 	return file
 }
